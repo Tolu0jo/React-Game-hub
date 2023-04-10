a46aba2b757b3f50c032668e497361ffc2dedb84
ToluOjo <ojotolulope137@gmail.com>
2023-04-10 08:14:44 +0100

    installing chakraUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-
+import { useState } from 'react'
 import App from './App'
 import './index.css'
 
