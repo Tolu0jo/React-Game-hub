5b017174ad89ddeac316b761c6a2bd9c6aa7afa3
ToluOjo <ojotolulope137@gmail.com>
2023-04-10 19:01:55 +0100

    Critic score dispaly

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -2,7 +2,7 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform{
+export  interface Platform{
     id:number;
     name:string;
     slug:string
@@ -11,7 +11,8 @@ export interface Game{
     id:number,
     name:string,
     background_image:string;
-    parent_platforms:{platform:Platform}[]
+    parent_platforms:{platform:Platform}[];
+    metacritic:number
 }
 interface FetchGame{
     count:number,
