9a6ad93fe0350d32bb8a51266182da9e25116354
ToluOjo <ojotolulope137@gmail.com>
2023-04-11 11:21:57 +0100

    Card Game cleanup

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,11 +14,11 @@ const GameCard = ({game}:Props) => {
     <Card >
         <Image src={croppedImage(game.background_image)}/>
         <CardBody>
-            <Heading fontSize="2xl">{game.name}</Heading>
-            <HStack justifyContent={"space-between"}>
+            <HStack justifyContent={"space-between"} marginBottom={3}>
                <PlatformiconList platforms={game.parent_platforms.map(p=>p.platform)}/>
             <CriticScore score={game.metacritic}/> 
             </HStack>
+             <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
     </Card>
   )
