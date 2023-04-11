a27fca4d9f0334f0a9716cc03f09135c1b5e3b11
ToluOjo <ojotolulope137@gmail.com>
2023-04-11 12:20:32 +0100

    Customise to get more blueish theme

--- src/blackTheme.ts
+++ src/blackTheme.ts
@@ -4,6 +4,21 @@ const config : ThemeConfig ={
     initialColorMode: "dark"
 }
 
-const theme =extendTheme({config})
+const theme =extendTheme({config
+,colors:{
+    goray:{
+        50:"#e7f3fe",
+        100:"#bbdaf7",
+        200:"#8fc2f1",
+        300:"#64abee",
+        400:"#4293eb",
+        500:"#317ad1",
+        600:"#265fa3",
+        700:"#1b4473",
+        800:"#0d2945",
+        900:"#010e18"
+    }
+}
+})
 
 export default theme;
